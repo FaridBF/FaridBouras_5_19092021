d9133247d29a111712c7e0204ed13ec66c3ea29a
Farid <bouras.farid@outlook.fr>
2021-10-02 21:39:19 +0200

    WIP tableau du panier

---
+++ panier.js
@@ -0,0 +1,11 @@
+// Création du tableau avec 6 élements via la class tableau
+var tableau = ["produit", "nom", "option", "quantité", "prix", "sous-total"];
+console.log("tableau", tableau);
+
+var input = document.getElementsByClassName("tableau");
+console.log(input);
+
+// for (var i = 0; i < input.length; i++) {
+//   // console.log(input[i].value);
+//   console.log(i);
+// }
