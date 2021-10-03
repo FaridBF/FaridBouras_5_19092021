33e5abdd3f24a2569f885936530f217f64075430
Farid <bouras.farid@outlook.fr>
2021-10-03 19:39:06 +0200

    Création de la fonction : createProduct

--- api.js
+++ api.js
@@ -1,8 +1,75 @@
 // Déclaration de l'URL de l'api
 const urlAPI = "http://localhost:3000/api/cameras";
 
-// Récupérer produits depuis l'API
+// Fonction pour afficher les données de l'API dans le DOM
+function createProduct(value) {
+  // récupérer div .total_produits
+  let total_produits = document.querySelector(".total_produits");
+  //créer div .produit
+  let produit = document.createElement("div");
+  produit.classList.add("produit");
+  total_produits.appendChild(produit);
 
+  // Création de la div image_produit
+  let image_produit = document.createElement("div");
+  image_produit.classList.add("image_produit");
+  produit.appendChild(image_produit);
+
+  // Création d'une balise img pour l'image
+  let image = document.createElement("img");
+  image.src = value[0].imageUrl; // ajout de l'attribut src avec la valeur récupérée de l'API
+  image.alt = value[0].name; // ajout du nom du produit dans alt
+  image_produit.appendChild(image); // ajout de la balise en tant qu'enfant dans la div parent
+
+  //créer div .texte_produit
+  let texte_produit = document.createElement("div");
+  texte_produit.classList.add("texte_produit");
+  produit.appendChild(texte_produit);
+
+  // créer h2 .nom_produit (enfant de .texte_produit)
+  let nom_produit = document.createElement("h2");
+  nom_produit.classList.add("nom_produit");
+  texte_produit.appendChild(nom_produit);
+  nom_produit.innerHTML = value[0].name;
+
+  // créer div
+  let descriptif_container = document.createElement("div");
+  texte_produit.appendChild(descriptif_container);
+
+  // créer p .descriptif_produit (enfant de div)
+  let descriptif_produit = document.createElement("p");
+  descriptif_produit.classList.add("descriptif_produit");
+  descriptif_container.appendChild(descriptif_produit);
+  descriptif_produit.innerHTML = value[0].description;
+
+  // créer div
+  let prix_container = document.createElement("div");
+  texte_produit.appendChild(prix_container);
+
+  // créer p .prix (enfant de div)
+  let prix = document.createElement("p");
+  prix.classList.add("prix");
+  prix_container.appendChild(prix);
+  prix.innerHTML = value[0].price;
+
+  // créer div .bouton
+  let bouton_conteneur = document.createElement("div");
+  bouton_conteneur.classList.add("bouton_conteneur");
+  texte_produit.appendChild(bouton_conteneur);
+
+  // création de la balise form avec action vers produit.html
+  let form_bouton = document.createElement("form");
+  form_bouton.action = "produit.html";
+  bouton_conteneur.appendChild(form_bouton);
+
+  // création du button comme enfant de parent form
+  let bouton_commander = document.createElement("button");
+  bouton_commander.classList.add("bouton_commander");
+  bouton_commander.innerHTML = "Acheter ce produit";
+  form_bouton.appendChild(bouton_commander);
+}
+
+// Récupérer produits depuis l'API
 function getProducts() {
   // utiliser fetch pour récupération données API
   fetch(urlAPI)
@@ -15,19 +82,8 @@ function getProducts() {
     // Récupérer la valeur de la réponse du JSON
     .then(function (value) {
       // console.log("MES PRODUITS", value[0]);
-      // Récupérer name, description, price, imageUrl
-      // et les afficher dans le DOM
       console.log(value);
-      document.querySelector(".nom_produit").innerHTML = value[0].name;
-      document.querySelector(".descriptif_produit").innerHTML =
-        value[0].description;
-      document.querySelector(".prix").innerHTML = value[0].price;
-
-      // Création d'une balise img pour l'image
-      let image = document.createElement("img");
-      image.src = value[0].imageUrl; // ajout de l'attribut src avec la valeur récupérée de l'API
-      image.alt = value[0].name; // ajout du nom du produit dans alt
-      document.querySelector(".image_produit").appendChild(image); // ajout de la balise en tant qu'enfant dans la div parent
+      createProduct(value);
     });
 }
 
