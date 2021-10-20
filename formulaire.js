b381138db3d9d1186b65cefbce1be2b575b759ed
Farid <bouras.farid@outlook.fr>
2021-10-20 10:37:11 +0200

    Gestion de l'ajout au niveau des quantités

--- formulaire.js
+++ formulaire.js
@@ -5,30 +5,73 @@
 // let shoppingCart = new ShoppingCart();
 // shoppingCart.getShoppingContent();
 // console.log(shoppingCart.content);
-
-shoppingCart.content.forEach((element) => createProduct(element));
+// shoppingCart.content.forEach((element) => createProduct(element));
 // ------------- FIN CLASSE GERANT PANIER DANS LOCALSTORAGE ------------- //
 
 // on récupère l'ensemble des données de manière brut ce qui correspond à notre contactobject en données brut
-let form = document.getElementById("contact-form"); // nom de notre formulaire dans la partie HTML (récupération via ID)
-let formContact = new FormData(document.getElementById("contact-form")); // Form Data est une classe permettant de transformer le formulaire dans une forme utilisable facilement
+// let form = document.getElementById("contact-form"); // nom de notre formulaire dans la partie HTML (récupération via ID)
+// let formContact = new FormData(document.getElementById("contact-form")); // Form Data est une classe permettant de transformer le formulaire dans une forme utilisable facilement
 
-let firstname = formContact.get("firstname"); // On récupère via les champs "name" dans le formulaire afin de récupérer la valeur dans le bonne forme (Formdata)
-let lastname = formContact.get("lastname");
-let city = formContact.get("city");
-let address = formContact.get("address");
-let email = formContact.get("email");
+// let firstname = formContact.get("firstname"); // On récupère via les champs "name" dans le formulaire afin de récupérer la valeur dans le bonne forme (Formdata)
+// let lastname = formContact.get("lastname");
+// let city = formContact.get("city");
+// let address = formContact.get("address");
+// let email = formContact.get("email");
 
-//Création du contact object via les données brut afin de formater l'ensemble pour l'API (il faut respecter exactement ce que demande l'API)
-let contactObject = {
-  firstName: firstname,
-  lastName: lastname,
-  city: city,
-  address: address,
-  email: email,
-};
+// //Création du contact object via les données brut afin de formater l'ensemble pour l'API (il faut respecter exactement ce que demande l'API)
+// let contactObject = {
+//   firstName: firstname,
+//   lastName: lastname,
+//   city: city,
+//   address: address,
+//   email: email,
+// };
 
-// const buttonClear = document.getElementById("clear");
-// buttonClear.addEventListener("click", () => {
-//   location.reload();
-// });
+// Récupération des ID, mise en place des écouteurs d'évènements
+document
+  .querySelector("#contact-form")
+  .addEventListener("submit", function (e) {
+    var nom = document.querySelector("#nom");
+    if (nom.value.length != 10) {
+      // alert("Merci de renseigner votre nom");
+      e.preventDefault; // pour empêcher la soumission du formulaire
+    }
+  });
+document
+  .querySelector("#contact-form")
+  .addEventListener("submit", function (e) {
+    var prenom = document.querySelector("#prenom");
+    if (prenom.value.length != 10) {
+      // alert("Merci de renseigner votre prénom");
+      e.preventDefault; // pour empêcher la soumission du formulaire
+    }
+  });
+document
+  .querySelector("#contact-form")
+  .addEventListener("submit", function (e) {
+    var adresse = document.querySelector("#adresse");
+    if (adresse.value.length != 10) {
+      // alert(
+      //   "Veuillez correctement renseigner le formulaire pour valider votre commande"
+      // );
+      e.preventDefault; // pour empêcher la soumission du formulaire
+    }
+  });
+document
+  .querySelector("#contact-form")
+  .addEventListener("submit", function (e) {
+    var ville = document.querySelector("#ville");
+    if (ville.value.length != 5) {
+      // alert("Merci de renseigner votre ville");
+      e.preventDefault; // pour empêcher la soumission du formulaire
+    }
+  });
+document
+  .querySelector("#contact-form")
+  .addEventListener("submit", function (e) {
+    var mail = document.querySelector("#mail");
+    if (mail.value.length != 5) {
+      alert("Merci de renseigner une adresse mail");
+      e.preventDefault; // pour empêcher la soumission du formulaire
+    }
+  });
--- panier.js
+++ panier.js
@@ -39,45 +39,27 @@ function createProduct(data) {
 
   // boucler sur le tableau_contenu data.lenses
   // pour chaque élément de data.lenses
-  data.lenses.forEach((element) => {
-    // --- creer une balise option
-    selection_option = document.createElement("option");
-    // assigner à cette balise la valeur de l'élément
-    selection_option.innerHTML = element;
-    // ajouter à la balise option un attribut value et lui assigner aussi la valeur de l'élément
-    selection_option.value = element;
-    // ajouter cette balise en tant qu'enfant de la balise select
-    selection.appendChild(selection_option);
-  });
+  // data.lenses.forEach((element) => {
+  //   // --- creer une balise option
+  //   selection_option = document.createElement("option");
+  //   // assigner à cette balise la valeur de l'élément
+  //   selection_option.innerHTML = element;
+  //   // ajouter à la balise option un attribut value et lui assigner aussi la valeur de l'élément
+  //   selection_option.value = element;
+  //   // ajouter cette balise en tant qu'enfant de la balise select
+  //   selection.appendChild(selection_option);
+  // });
 
   // créer div .quantite_container_panier (enfant de .shopping_details)
   let quantite_container_panier = document.createElement("td");
   quantite_container_panier.classList.add("quantite_container_panier");
   shopping_details.appendChild(quantite_container_panier);
 
-  // créer p .descriptif_quantite (enfant de .quantite_container_panier)
-  let descriptif_quantite_panier = document.createElement("p");
-  descriptif_quantite_panier.classList.add("descriptif_quantite_panier");
-  quantite_container_panier.appendChild(descriptif_quantite_panier);
-  descriptif_quantite_panier.innerHTML = "Quantité : ";
-
-  // créer une class quantite_panier (enfant de .quantite_container_panier)
-  let quantite_panier = document.createElement("select");
+  // créer une class quantite_panier
+  let quantite_panier = document.createElement("p");
   quantite_panier.classList.add("quantite_panier");
   quantite_container_panier.appendChild(quantite_panier);
-
-  // boucler sur le tableau_contenu array afin d'indiquer la quantité
-  const array = ["1", "2", "3", "4", "5"];
-  array.forEach((element) => {
-    // creer une balise quantite_option
-    quantite_option_panier = document.createElement("option");
-    // assigner à cette balise la valeur de l'élément
-    quantite_option_panier.innerHTML = element;
-    // ajouter à la balise option un attribut value et lui assigner aussi la valeur de l'élément
-    quantite_option_panier.value = element;
-    // ajouter cette balise en tant qu'enfant de la balise quantite
-    quantite_panier.appendChild(quantite_option_panier);
-  });
+  quantite_panier.innerHTML = data.quantity;
 
   // créer prix enfant de shoppings details
   let prix_panier = document.createElement("td");
@@ -104,42 +86,37 @@ class ShoppingCart {
 
   add(oneProduct) {
     this.getShoppingContent();
-    this.content.push(oneProduct);
-    // transforme JSON en string
-    localStorage.setItem(this.nameInStorage, JSON.stringify(this.content));
+    //récupération du contenu du panier avant qu'il ne puisse pouvoir rentrer dans la fonction
+    // j'ajoute un produit
+    let alreadyExists = false;
+    // boucler sur chaque élément du panier
+    this.content.forEach(function (oneElement) {
+      // permet de faire le tour de l'ensemble des éléments dans notre panier
+      if (oneElement._id === oneProduct._id) {
+        // si sur chaque élement sur lequel on itère l'id de l'élément correspond à l'id du produit a ajouté
+        alreadyExists = true; // condition permettant d'indiquer qu'elle a trouvé une quantité à incrémenter
+        oneElement.quantity += 1; // alors j'incrémente la quantité de 1 si la condition  précèdente est remplie
+      }
+    });
+    if (alreadyExists === false) {
+      // s'il n'est pas dans ma condition de ma quantité, elle n'a donc pas trouvé la valeur, de quantité à incrémenter, donc ajout
+      let productToSave = {
+        // Variable qui contient tous les éléments que je souhaite sauvegarder
+        _id: oneProduct._id,
+        name: oneProduct.name,
+        imageUrl: oneProduct.imageUrl,
+        quantity: 1, //quantité que je mets à 1 par default
+        price: oneProduct.price,
+      };
+      this.content.push(productToSave); //j'envoie l'ensemble des éléments sauvegarder dans mon panier en faisant le push du produit en question
+    }
+    localStorage.setItem(this.nameInStorage, JSON.stringify(this.content)); // transforme JSON en string
   }
 }
-
 // Lien avec le fichier class.js (permet d'afficher les éléments dans panier)
 let shoppingCart = new ShoppingCart();
 shoppingCart.getShoppingContent();
-console.log(shoppingCart.content);
+console.log("shoppingCart.content", shoppingCart.content);
 
 shoppingCart.content.forEach((element) => createProduct(element));
 // ------------- FIN CLASSE GERANT PANIER DANS LOCALSTORAGE ------------- //
-
-// add(oneProduct) {
-//   // j'ajoute un produit
-//   let alreadyExists = false;
-//   this.content.forEach(function (oneElement) {
-//     // permet de faire le tour de l'ensemble des éléments dans notre panier
-//     if (oneElement.data._id === oneProduct.data._id) {
-//       // si sur chaque élement sur lequel on itère l'id de l'élément correspond à l'id du produit a ajouté
-//       alreadyExists = true; // condition permettant d'indiquer qu'elle a trouvé une quantité à incrémenter
-//       oneElement.quantite_option_panier += 1; // alors j'incrémente la quantité de 1 si la condition  précèdente est remplie
-//     }
-//   });
-//   if (alreadyExists === false)
-//     // s'il n'est pas dans ma condition de ma quantité, elle n'a donc pas trouvé la valeur, de quantité à incrémenter, donc ajout
-//     var save = {
-//       // Variable qui contient tous les éléments que je souhaite sauvegarder
-//       reference_panier: oneProduct.data.name,
-//       // imageUrl: oneProduct.data.imageUrl,
-//       // quantite_option_panier: 1, // quantité que je mets à 1 par default
-//       // price: oneProduct.data.price,
-//       _id: oneProduct.data._id,
-//     };
-//   this.getShoppingContent();
-//   this.content.push(save); //j'envoie l'ensemble des éléments sauvegarder dans mon panier en faisant le push du produit en question
-//   localStorage.setItem(this.nameInStorage, JSON.stringify(this.content)); // transforme JSON en string
-// }
