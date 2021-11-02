984789106cedd3d5d1d7e7297ac6466a6680d249
Farid <bouras.farid@outlook.fr>
2021-11-02 21:52:25 +0100

    Ajout de la ville dans formulaire de contact et refacto

--- js/recap_commande.js
+++ js/recap_commande.js
@@ -1,31 +1,27 @@
 function display_recap_commande() {
   // récupération montant total de la commande
   let resultat_total = localStorage.getItem("totalInStorage");
-  console.log(resultat_total); // TODO: a suppr
-  // récupération infos du serveur (order_id, email et address du contact)
+  // récupération infos du serveur (order_id, email, city et address du contact)
   let orderId = localStorage.getItem("orderId");
   let contactAddress = localStorage.getItem("contactAddress");
   let contactEmail = localStorage.getItem("contactEmail");
-  console.log(orderId);
-  console.log(contactAddress);
-  console.log(contactEmail);
+  let contactCity = localStorage.getItem("contactCity");
 
-  //   let shoppingCart = new ShoppingCart();
-  //   shoppingCart.getShoppingContent();
   const recap_commande = document.querySelector("#details_commande");
   recap_commande.innerHTML = `
-  <p class="texte_details_commande">Merci , votre commande n° ${orderId}
+  <p class="texte_details_commande">Merci , votre commande n° <b>${orderId}</b>
       est validée ! </p>
-  <p class="texte_details_commande">Votre facture pour un montant total de ${resultat_total} va vous
-      être transmise par mail à : ${contactEmail}</p>
-  <p class="texte_details_commande">On vous préviendra quand elle partira chez vous à l'adresse suivante :
+  <p class="texte_details_commande">Votre facture pour un montant total de <b>${resultat_total} euros</b>  va vous
+      être transmise par mail à : <b>${contactEmail}</b></p>
+  <p class="texte_details_commande">On vous préviendra quand elle partira chez vous à l'adresse suivante :<b>
   <p class="texte_details_commande">
-  ${contactAddress}</p>
+  ${contactAddress}</b> à <b> ${contactCity}</b></p>
   </p>`;
   // vider les éléments du localStorage
   localStorage.setItem("orderId", "");
   localStorage.setItem("contactAddress", "");
   localStorage.setItem("contactEmail", "");
+  localStorage.setItem("contactCity", "");
   localStorage.setItem("totalInStorage", "");
 }
 
